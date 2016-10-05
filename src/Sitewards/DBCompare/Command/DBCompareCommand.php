ae3e5b41c476658f8a215db6457a4b00032016ad
David Manners <david.manners@sitewards.com>
2016-10-05 08:42:54 +0000

    Add the file path questions for the main and merging databases

--- src/Sitewards/DBCompare/Command/DBCompareCommand.php
+++ src/Sitewards/DBCompare/Command/DBCompareCommand.php
@@ -11,11 +11,12 @@ namespace Sitewards\DBCompare\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 
 class DBCompareCommand extends Command
 {
     /**
-     * Set-up the db:comapre command
+     * Set-up the db:compare command
      */
     protected function configure()
     {
@@ -34,6 +35,31 @@ class DBCompareCommand extends Command
      */
     protected function execute(InputInterface $oInput, OutputInterface $oOutput)
     {
-        $oOutput->writeln('db:compare run successfully');
+        $oOutput->writeln('Staring the db:compare');
+
+        $sMainDBPath = $this->getFilePath($oInput, $oOutput, 'Please enter the main database file path:');
+        $sMergingDB  = $this->getFilePath($oInput, $oOutput, 'Please enter the merging database file path:');
+
+        $oOutput->writeln('Main DB file: ' . $sMainDBPath);
+        $oOutput->writeln('Merging DB file: ' . $sMergingDB);
+        $oOutput->writeln('Ending the db:compare');
+    }
+
+    /**
+     * TODO: add file path validation
+     *
+     * @param InputInterface $oInput
+     * @param OutputInterface $oOutput
+     * @param string $sQuestion
+     * @return string
+     */
+    private function getFilePath(
+        InputInterface $oInput,
+        OutputInterface $oOutput,
+        $sQuestion = ''
+    ) {
+        $oQuestionHelper   = $this->getHelper('question');
+        $oFilePathQuestion = new Question($sQuestion);
+        return $oQuestionHelper->ask($oInput, $oOutput, $oFilePathQuestion);
     }
 }
