d01be5916a545006a3c1058a702c3c21228051a1
David Manners <david.manners@sitewards.com>
2016-10-05 09:11:11 +0000

    Ask user what item they would like to merge

--- src/Sitewards/DBCompare/Command/DBCompareCommand.php
+++ src/Sitewards/DBCompare/Command/DBCompareCommand.php
@@ -12,6 +12,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 use Sitewards\DBCompare\Exception\FileNotFoundException;
 
 class DBCompareCommand extends Command
@@ -38,11 +39,13 @@ class DBCompareCommand extends Command
     {
         $oOutput->writeln('Staring the db:compare');
 
-        $sMainDBPath = $this->getFilePath($oInput, $oOutput, 'Please enter the main database file path:');
-        $sMergingDB  = $this->getFilePath($oInput, $oOutput, 'Please enter the merging database file path:');
+        $sMainDBPath  = $this->getFilePath($oInput, $oOutput, 'Please enter the main database file path:');
+        $sMergingDB   = $this->getFilePath($oInput, $oOutput, 'Please enter the merging database file path:');
+        $sItemToMerge = $this->getItemToMerge($oInput, $oOutput);
 
         $oOutput->writeln('Main DB file: ' . $sMainDBPath);
         $oOutput->writeln('Merging DB file: ' . $sMergingDB);
+        $oOutput->writeln('Merging item : ' . $sItemToMerge);
         $oOutput->writeln('Ending the db:compare');
     }
 
@@ -68,5 +71,22 @@ class DBCompareCommand extends Command
             }
         );
         return $oQuestionHelper->ask($oInput, $oOutput, $oFilePathQuestion);
+    }
+
+    /**
+     * @param InputInterface $oInput
+     * @param OutputInterface $oOutput
+     * @return string
+     */
+    private function getItemToMerge(InputInterface $oInput, OutputInterface $oOutput)
+    {
+        $oQuestionHelper = $this->getHelper('question');
+        $oItemQuestion   = new ChoiceQuestion(
+            'Please select the item you wish to merge',
+            ['cms pages', 'cms blocks', 'system config'],
+            '0'
+        );
+
+        return $oQuestionHelper->ask($oInput, $oOutput, $oItemQuestion);
     }
 }
