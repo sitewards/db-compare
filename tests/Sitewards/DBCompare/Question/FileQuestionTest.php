eda399647ee0491638c1ed25b44c22fa60d804f8
David Manners <david.manners@sitewards.com>
2016-10-13 12:19:25 +0000

    Add test for the file path validator

--- tests/Sitewards/DBCompare/Question/FileQuestionTest.php
+++ tests/Sitewards/DBCompare/Question/FileQuestionTest.php
@@ -6,8 +6,6 @@ use Sitewards\DBCompare\Validator\FilePath;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\Input;
 use Symfony\Component\Console\Output\Output;
-use Sitewards\DBCompare\Exception\FileNotFoundException;
-use Sitewards\DBCompare\Exception\FileNotReadableException;
 
 class FileQuestionTest extends \PHPUnit_Framework_TestCase
 {
@@ -38,33 +36,5 @@ class FileQuestionTest extends \PHPUnit_Framework_TestCase
             'Test Question'
         );
         $this->assertEquals('sample.sql', $sWorkerName);
-    }
-
-    /**
-     * @throws \PHPUnit_Framework_Exception
-     * @throws \Symfony\Component\Console\Exception\InvalidArgumentException
-     * @throws \Symfony\Component\Console\Exception\LogicException
-     */
-    public function testGetMergeWorkerFileNotFoundException()
-    {
-        $oQuestionHelper = $this->getMockBuilder(QuestionHelper::class)->getMock();
-        $oQuestionHelper->expects($this->once())
-            ->method('ask')
-            ->willThrowException(new FileNotFoundException());
-
-        $this->expectException(FileNotFoundException::class);
-        $oInput = $this->createMock(Input::class);
-        $oOutput = $this->createMock(Output::class);
-        $oValidator      = $this->createMock(FilePath::class);
-
-        $oWorkerQuestion = new FileQuestion(
-            $oQuestionHelper,
-            $oValidator
-        );
-        $oWorkerQuestion->getFilePath(
-            $oInput,
-            $oOutput,
-            'Test Question'
-        );
     }
 }
